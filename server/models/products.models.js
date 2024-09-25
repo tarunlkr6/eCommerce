4acb96401f9c3006ba0a7dba3a4d8ae9c288206c
Animus <tarunkumarlkr6@gmail.com>
2024-09-25 19:20:46 +0530

    defined schema for user, product and order, added user schema methods

---
+++ server/models/products.models.js
@@ -0,0 +1,79 @@
+import mongoose, { Schema } from "mongoose"
+
+const reviewSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    rating: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    comment: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true })
+
+const productSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    image: [
+        {
+            publicId: {
+                type: String,
+                required: true,
+            },
+            // cloudinary url
+            url: {
+                type: String,
+                required: true,
+            },
+        },
+    ],
+    price: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    brand: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    ratings: {
+        type: Number,
+        default: 0,
+    },
+    reviews: [reviewSchema],
+    numReviews: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+}, { timestapms: true }
+)
+
+export const Product = mongoose.model("Product", productSchema)
