3a503e1ea88579060980ca424996045c666e8d8c
Animus <tarunkumarlkr6@gmail.com>
2024-09-26 22:49:15 +0530

    implemented user registration controller with validation and error handling

--- server/models/user.models.js
+++ server/models/user.models.js
@@ -29,14 +29,14 @@ const userSchema = new Schema({
     },
     refreshToken: {
         type: String,
-        required: true,
     },
 }, { timestamps: true })
 
 userSchema.pre("save", async function (next) {
-    if (!this.modified("password")) return next()
-
-    this.password = bcrypt.hash(this.password, 10)
+    if (!this.isModified("password")) {
+        return next()
+    }
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,6 +3,6 @@ import { registerUser } from "../controllers/user.controllers.js"
 
 const router = Router()
 
-router.route("/register").get(registerUser)
+router.route("/register").post(registerUser)
 
 export default router
